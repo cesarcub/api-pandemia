354ee47a5836018a78588b75711ed5341331c0a9
Estudiante Politecnico Grancolombiano <general@poligran.edu.co>
2020-03-13 19:32:53 -0500

    Se crea la muestra de la data a mostrar en JSON

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const pandemias = require('./data/pandemia');
 
-app.listen(3000, () =>{
+
+//Se levanta servidor de express
+app.listen(3000, () => {
     console.log('Corriendo en http://localhost:3000');
-})
+});
+
+//1. Crear nueva pandemia, esta se debe crear solamente con la información básica
+app.post('/crear', (req, res) => {
+    console.log(req.body);
+    const pandemia = req.body;
+    res.send('Todo bien');
+});
