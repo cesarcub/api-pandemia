5118f4f9e41e106a906f46ace6dca05b361e226c
Estudiante Politecnico Grancolombiano <general@poligran.edu.co>
2020-03-13 20:48:47 -0500

    Se crea método para crear nueva pandemia, para insertar y actualizar información de un país, para consultar paises de una pandemia y para consultar el detalle de una pandemia

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const pandemias = require('./data/pandemia');
+let pandemias = require('./data/pandemia');
 
+//Se declara middleware para recibir JSON
+app.use(express.json());
 
 //Se levanta servidor de express
 app.listen(3000, () => {
@@ -10,7 +12,91 @@ app.listen(3000, () => {
 
 //1. Crear nueva pandemia, esta se debe crear solamente con la información básica
 app.post('/crear', (req, res) => {
-    console.log(req.body);
-    const pandemia = req.body;
-    res.send('Todo bien');
+    //Se recibe body y se asigna a la variable pandemia
+    const { body: pandemia } = req;
+
+    //Se crea ID autoincrementable
+    const id = pandemias.length + 1;
+    const newPandemia = {
+        id,
+        ...pandemia
+    }
+
+    pandemias.push(newPandemia);
+
+    //Se retornan todas las pandemias
+    res.send(pandemias);
 });
+
+//2. Insertar y actualizar información de un país dado el id de una pandemia
+app.put('/pandemia/crear-pais/:idPandemia', (req, res) => {
+    //Se recibe ID de pandemia
+    const { idPandemia } = req.params;
+
+    //Se recibe body y se asigna a la variable pandemia
+    const { body: pais } = req;
+
+    const pandemia = pandemias.find((pandemia) => pandemia.id === Number(idPandemia));
+    if (!pandemia) {
+        res.send('ID de pandemia no existe');
+    }
+
+    //Se crea ID autoincrementable
+    const idPais = pandemia.paises.length + 1;
+
+    //Se crea nuevo objeto de país
+    const newPais = {
+        id: idPais,
+        ...pais
+    }
+
+    //Se inserta país a la pandemia
+    pandemia.paises.push(newPais);
+
+    //Se retornan la pandemia con el nuevo país
+    res.send(pandemias);
+
+});
+
+//3. Consultar todos los países de una pandemia con su información dado el id de la pandemia
+app.get('/pandemia/consultar-pais/:idPandemia', (req, res) => {
+    //Se recibe ID de pandemia
+    const { idPandemia } = req.params;
+
+    const pandemia = pandemias.find((pandemia) => pandemia.id === Number(idPandemia));
+    if (!pandemia) {
+        res.send('ID de pandemia no existe');
+    }
+
+
+    //Se retornan los paises de la pandemia
+    res.send(pandemia.paises);
+
+});
+
+/*
+    4. Consultar la información de una pandemia (ID, Nombre, Síntomas, Recomendaciones)
+    incluyendo cantidad de países, cantidad de infectados, recuperados y de muertes en total de
+    una pandemia, esto debe ser calculado en base a la información reportada por cada país. Y que
+    retorne la información de todos los países asociados a esta pandemia con su información
+*/
+
+app.get('/pandemia/consultar/:idPandemia', (req, res) => {
+    //Se recibe ID de pandemia
+    const { idPandemia } = req.params;
+
+    const pandemia = pandemias.find((pandemia) => pandemia.id === Number(idPandemia));
+    if (!pandemia) {
+        res.send('ID de pandemia no existe');
+    }
+
+    //Se retornan la pandemia con los paises
+    res.send(pandemia);
+
+});
+
+/*
+    Curar un país por id de pandemia y id del país, esto tendrá el efecto de que el país sumará a los
+    curados la cantidad d de infectados menos la cantidad de muertos y cambiará el estado de en
+    curso de verdadero a falso.
+*/
--- data/pandemia.js
+++ data/pandemia.js
@@ -1,26 +1,26 @@
 const pandemias = [
     {
         id: 1,
-        nombre: 'Coronavirus',
-        sintomas: 'Dolor de cabeza, sensación de gripa, flemas amarillas, dolor en el pecho',
+        nombre: "Coronavirus",
+        sintomas: "Dolor de cabeza, sensación de gripa, flemas amarillas, dolor en el pecho",
         recomendaciones:  [
-            'Beber mucha agua',
-            'Lavarse las manos constantemente',
-            'Dormir bien',
-            'Quedarse en casa'
+            "Beber mucha agua",
+            "Lavarse las manos constantemente",
+            "Dormir bien",
+            "Quedarse en casa"
         ],
         enCurso: true,
         paises: [
             {
                 id: 1,
-                nombre: 'China',
+                nombre: "China",
                 infectados: 141000,
                 recuperados: 136770,
                 muertes: 2000
             },
             {
                 id: 2,
-                nombre: 'Colombia',
+                nombre: "Colombia",
                 infectados: 3,
                 recuperados: 2,
                 muertes: 0
@@ -29,26 +29,26 @@ const pandemias = [
     },
     {
         id: 2,
-        nombre: 'SARS',
-        sintomas: 'Fiebre alta, dolor de cabeza, dolor en el cuerpo',
+        nombre: "SARS",
+        sintomas: "Fiebre alta, dolor de cabeza, dolor en el cuerpo",
         recomendaciones:  [
-            'Beber agua en abundancia',
-            'Lavarse las manos',
-            'Quedarse en casa',
-            'Usar antibacterial'
+            "Beber agua en abundancia",
+            "Lavarse las manos",
+            "Quedarse en casa",
+            "Usar antibacterial"
         ],
         enCurso: true,
         paises: [
             {
                 id: 1,
-                nombre: 'Estados Unidos',
+                nombre: "Estados Unidos",
                 infectados: 400,
                 recuperados: 250,
                 muertes: 200
             },
             {
                 id: 2,
-                nombre: 'Brasil',
+                nombre: "Brasil",
                 infectados: 2,
                 recuperados: 2,
                 muertes: 0
